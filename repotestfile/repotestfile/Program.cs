5e44d5dc454116575f3529956d081ada9d871f59
Ezekiel Tyus <EJTyus@student.fullsail.edu>
2020-10-06 13:52:48 -0400

    i fixed all the shit i just broke

--- repotestfile/repotestfile/Program.cs
+++ repotestfile/repotestfile/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Dynamic;
 
 namespace repotestfile
 {
@@ -6,10 +7,34 @@ namespace repotestfile
     {
         static void Main(string[] args)
         {
-            int guess;
-            var rand = new Random();
-            rand.Next(100, 200);
            
+            var rand = new Random();
+           int random = rand.Next(100, 200);
+            bool game = true;
+            while (game)
+            {
+            Console.WriteLine("Guess a number between 100 and 200");
+            string guess = Console.ReadLine();
+            int result = Int32.Parse(guess);
+
+            if (result < random)
+            {
+                Console.WriteLine("the result number is too low");
+                    game = true;
+                  
+            }
+            else if(result > random)
+                {
+                Console.WriteLine("the result is too high");
+                    game = true;
+                  
+                }
+            else
+            {
+                Console.WriteLine($"your result is correct the number was{random}");
+                    game = false;
+            }
+            }
         }
     }
 }
