55c0c368f59d174bae45efc7d0365e028aef019d
Ezekiel Tyus <EJTyus@student.fullsail.edu>
2020-10-06 13:32:17 -0400

    variable declarations

---
+++ repotestfile/repotestfile/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace repotestfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int guess;
+            var rand = new Random();
+            rand.Next(100, 200);
+           
+        }
+    }
+}
